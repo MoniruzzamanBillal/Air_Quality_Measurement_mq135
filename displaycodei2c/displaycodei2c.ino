ec26c8fa23bbbe04d8db396c0f621f331530382f
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-09-04 09:35:59 +0600

    display code

---
+++ displaycodei2c/displaycodei2c.ino
@@ -0,0 +1,28 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+}
+
+void loop() {
+
+
+  lcd.setCursor(0, 0);
+  lcd.print("Hello World!!!");
+
+  delay(2000);
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Testing l2c");
+  lcd.setCursor(0, 1);
+  lcd.print("Module display!!!");
+
+  delay(2000);
+  lcd.clear();
+}
