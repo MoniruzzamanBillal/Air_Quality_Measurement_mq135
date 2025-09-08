bd3d683d0cb8236d5372d4f49526b0cdeb8f48c0
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-09-08 19:08:30 +0600

    project prototype 1.2

---
+++ MainCode/MainCode.ino
@@ -0,0 +1,110 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <DHT.h>
+
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+int mq135Pin = A0;
+
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// scl pin --> A5
+// sda pin --> A4
+
+
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+}
+
+void loop() {
+
+
+  int mq135Value = analogRead(mq135Pin);
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+
+
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("⚠️ Failed to read from DHT11 sensor!");
+    delay(2000);
+    return;
+  }
+
+
+
+  String airStatus;
+  if (mq135Value < 200) {
+    airStatus = "GOOD Air";
+  } else if (mq135Value >= 200 && mq135Value < 400) {
+    airStatus = "MODERATE Air";
+  } else {
+    airStatus = "POOR Air!";
+  }
+
+
+  // ----- Show on Serial -----
+  Serial.println("------ Environment Status ------");
+  Serial.print("Air Quality (MQ135): ");
+  Serial.print(mq135Value);
+  Serial.print(" → ");
+  Serial.println(airStatus);
+
+  Serial.print("Temperature (DHT11): ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+
+  Serial.print("Humidity (DHT11): ");
+  Serial.print(humidity);
+  Serial.println(" %");
+
+  Serial.println("--------------------------------------");
+  Serial.println();
+
+
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("AirQ Val=");
+  lcd.print(mq135Value);
+  lcd.setCursor(0, 1);
+  lcd.print(airStatus);
+  delay(3000);
+
+
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("T=");
+  lcd.print(temperature);
+  lcd.print("C H=");
+  lcd.print(humidity);
+  lcd.print("%");
+
+  lcd.setCursor(0, 1);
+  if (temperature > 30) {
+    lcd.print("Status: HOT");
+  } else if (temperature < 15) {
+    lcd.print("Status: COLD");
+  } else if (humidity > 70) {
+    lcd.print("Humid!");
+  } else if (humidity < 30) {
+    lcd.print("Dry Air");
+  } else {
+    lcd.print("Status: OK");
+  }
+  delay(3000);
+
+
+
+
+  lcd.clear();
+}
--- mq135sensorCode/mq135sensorCode.ino
+++ mq135sensorCode/mq135sensorCode.ino
@@ -1,29 +1,50 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 int mq135Pin = A0;
+int mqdigital = 3;
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 
 void setup() {
+   pinMode(mqdigital, INPUT);
   Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
 }
 
 void loop() {
+ 
   int sensorValue = analogRead(mq135Pin);
+   int digitalValue  = digitalRead(mqdigital);
 
 
 
   Serial.print("MQ-135 Value: ");
   Serial.print(sensorValue);
+   Serial.println(" ");
 
-  Serial.print("  |  Air Quality: ");
+     Serial.print("digitalValue: ");
+  Serial.print(digitalValue);
+Serial.println(" ");
 
-  if (sensorValue < 300) {
-    Serial.println("Good 😊");
-  } else if (sensorValue < 600) {
-    Serial.println("Moderate 😐");
-  } else {
-    Serial.println("Poor 😷");
-  }
+
+  lcd.setCursor(0, 0);
+  lcd.print("sensor=" + sensorValue );
+  lcd.setCursor(0, 1);
+  lcd.print("digital=" + digitalValue );
+
+  // if (sensorValue < 300) {
+  //   Serial.println("Good 😊");
+  // } else if (sensorValue < 600) {
+  //   Serial.println("Moderate 😐");
+  // } else {
+  //   Serial.println("Poor 😷");
+  // }
 
 
   delay(500);
+    lcd.clear();
 }
